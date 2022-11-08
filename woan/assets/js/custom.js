d93d30553647a6281447df6232216ba3ba5ba142
Josh L. Minga <joshlminga@gmail.com>
2022-11-08 21:07:42 +0300

    menu and boarder

--- woan/assets/js/custom.js
+++ woan/assets/js/custom.js
@@ -6,7 +6,18 @@ if(chldHm){
     // DIV
     let hmb_menu = chldHm.querySelector('div.chld-nav-icon > i'); 
     hmb_menu.addEventListener('click', function(){
-        // Remove d-none class
+        
+        // Toggle Class
         menu.classList.toggle('d-none');
+        // Add Effects
+        menu.classList.add('animated', 'fadeInLeft');
+        // if class hmb_menu has class fa-bars then remove it and add fa-colse
+        if(hmb_menu.classList.contains('fa-bars')){
+            hmb_menu.classList.remove('fa-bars');
+            hmb_menu.classList.add('fa-close');
+        }else{
+            hmb_menu.classList.remove('fa-close');
+            hmb_menu.classList.add('fa-bars');
+        }
     });
 }
